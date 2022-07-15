a9b824cb26a31644294b1307f95cfc50831788a2
Queren Hapuque <queren.fernandes@yahoo.com>
2022-07-15 11:19:28 -0300

    Add files via upload

---
+++ Aula2/index.js
@@ -0,0 +1,3 @@
+console.log('Olá Mundo');
+console.log('Este trecho será exibido no console do navegador.');
+alert('Olá novamente!') //a função alert não está definida no node, ela vai funcionar apenas no navegador.
