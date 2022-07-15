f98c5e0d03c305737962e41cc362b47b9c37fe15
Queren Hapuque <queren.fernandes@yahoo.com>
2022-07-15 10:57:50 -0300

    Add files via upload

---
+++ Aula1/index.js
@@ -0,0 +1,8 @@
+/* Sempre que quisermos debugar o código, usamos o console.log */
+
+console.log("Queren 'Hapuque'"); // todos são string = texto
+console.log('Queren "Hapuque"'); // Não conseguimos colocar palavras entre aspas simples dentro do meu texto. 
+console.log(`'Queren' "Hapuque"`); // utilizado para fazer templates, pode usar tanto aspas simples quanto aspas duplas
+console.log(12, 34.56, 'Queren'); // apresenta as aspas porque tem números e string junto na mesma frase.
+
+/*Em Js não tem separação entre número float e int, tudo é number.*/
