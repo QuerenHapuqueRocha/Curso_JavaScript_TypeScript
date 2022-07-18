f3e4ac295fe175e13952fac6c13070e12f3f411a
Queren Hapuque <queren.fernandes@yahoo.com>
2022-07-18 16:19:40 -0300

    Add files via upload

---
+++ Aula4/index.js
@@ -0,0 +1,8 @@
+// ECMAScript 2015 (ES6) foi uma padronização que ocorreu na linguagem JS
+
+var nome = 'Queren';
+var nome = 'Rocha';
+
+console.log(nome);
+
+/*a variável substiui o valor original quando é declarada novamente usando o 'var' */
---
+++ Aula4/index2.js
@@ -0,0 +1,20 @@
+
+// Variáveis do tipo STRING
+const nome = "Luiz";
+const nome1 = 'Luiz';
+const nome2 = `Luiz`;
+
+// Variáveis do tipo NUMBER
+const num1 = 10;
+const num2 =10.52;
+
+//Variável Undefined - Não aponta pra local nenhum na memória
+let nomeAluno;
+
+//Variável Null - Não aponta pra local nenhum na memória
+let sobrenomeAluno = null;
+/*É usado quando queremos colocar o valor de uma variável como nulo. Ex.: Um site onde o usuário pode selecionar a cor de fundo, caso o usuário não escolha nenhum valor, a variável será null, se ele escolher a cor, a variável será a cor.*/
+
+//Variável do tipo booleana - tipo lógico
+const boolean = false;
+
